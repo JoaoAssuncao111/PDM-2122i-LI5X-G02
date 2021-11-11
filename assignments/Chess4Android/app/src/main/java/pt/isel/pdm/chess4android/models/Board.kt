93e70e1924154be04b5c5cf7b279a7fe7ec19835
A47181 <A47181@alunos.isel.pt>
2021-11-11 22:28:25 +0000

    Update Board to be supported by 2d Array

--- assignments/Chess4Android/app/src/main/java/pt/isel/pdm/chess4android/models/Board.kt
+++ assignments/Chess4Android/app/src/main/java/pt/isel/pdm/chess4android/models/Board.kt
@@ -1,13 +1,15 @@
 package pt.isel.pdm.chess4android.models
 
 import pt.isel.pdm.chess4android.Army
+import java.sql.RowId
 
 class Board() {
     val side: Int = 8
-    private var board = mutableSetOf<ChessPiece>()
+
+    private var board = Array<Array<ChessPiece?>>(side) { i -> Array(8) { j -> null } }
 
     private fun addToBoard(p: ChessPiece){
-        board.add(p)
+        board[p.row][p.column]
     }
     fun startGame() {
         for (i in 0 until 2){
@@ -25,12 +27,21 @@ class Board() {
     }
 
 
-    fun canMove(piece: ChessPiece){
+    fun canMove(piece: ChessPiece,newTile: Tile): Boolean{
+        if(piece.row == newTile.row && piece.column == newTile.column) return false
+        val allMoves = piece.myMoves()
+        repeat(allMoves.size){
+            if(!allMoves.iterator().next().contains(newTile)) return false
+        }
 
 
 
     }
 
+
+    fun pieceAtTile(tile: Tile): ChessPiece? {
+        return board[tile.row][tile.column]
+    }
 
 
 }
