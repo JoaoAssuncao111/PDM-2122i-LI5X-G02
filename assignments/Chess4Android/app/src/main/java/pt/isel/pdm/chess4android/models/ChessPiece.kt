683392d04895fbb14d65fcfa782fd484382f7c5d
A47181 <A47181@alunos.isel.pt>
2021-11-12 18:35:49 +0000

    Updated Pieces' moves logic

--- assignments/Chess4Android/app/src/main/java/pt/isel/pdm/chess4android/models/ChessPiece.kt
+++ assignments/Chess4Android/app/src/main/java/pt/isel/pdm/chess4android/models/ChessPiece.kt
@@ -10,7 +10,7 @@ abstract class ChessPiece (val army: Army, var row: Int, var column: Int) {
     fun directionalMove(range: Int, direction: Directions): List<Tile> {
         var list: MutableList<Tile> = mutableListOf()
         for (i in 1..range) {
-            if(row+direction.row in 1 until maxRange && column+direction.column in 1 until maxRange) list.add(Tile(row,column))
+            if(row+direction.row in 1 until maxRange && column+direction.column in 1 until maxRange) list.add(Tile(row+direction.row,column+direction.column))
       }
         return list
     }
--- assignments/Chess4Android/app/src/main/java/pt/isel/pdm/chess4android/models/Pawn.kt
+++ assignments/Chess4Android/app/src/main/java/pt/isel/pdm/chess4android/models/Pawn.kt
@@ -4,6 +4,7 @@ import pt.isel.pdm.chess4android.Army
 
 class Pawn(army: Army, row: Int, collum: Int) : ChessPiece(army, row, collum){
     var isFirstMove: Boolean = true
+
     override fun myMoves(): MutableList<List<Tile>> {
         var moves: MutableList<List<Tile>> = mutableListOf()
         if(isFirstMove) moves.add(directionalMove(2,Directions.UP)) else moves.add(directionalMove(1,Directions.UP))
